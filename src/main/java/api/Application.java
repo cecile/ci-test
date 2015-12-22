ecf8b81a5aae821eeb26c23e506a6b02da8244b9
Juan Medina <jamedina@gmail.com>
2015-12-22 11:42:32 +0000

    test

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -20,6 +20,7 @@ public class Application {
         Map<String, Object> body = new HashMap<>();
 
         body.put("status", "OK");
+        body.put("status2", "OK");
 
         return body;
     }
