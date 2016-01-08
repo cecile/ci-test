2bc671218f3b93d0b004a39e16807f6f3bd86e2e
Juan Medina <jamedina@gmail.com>
2016-01-08 17:49:56 +0000

    broken

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -19,7 +19,7 @@ public class Application {
 
         Map<String, Object> body = new HashMap<>();
 
-        body.put("status", "OK");
+        body.put("status", "OK2");
 
         return body;
     }
